a13487ab3e05479222366723d9c62c61e0afdc11
AndrewPunchak <68635339+AndrewPunchak@users.noreply.github.com>
2020-11-09 21:31:42 -0800

    changes

--- js/presave.js
+++ js/presave.js
@@ -3,7 +3,7 @@
 
 document.getElementById("ar-link").addEventListener("message", function (event) {
     if (event.data == "_apple_ar_quicklook_button_tapped") {
-        window.location.href="https://google.com";
+        window.location.href="https://open.spotify.com/track/2T5eh3UxH52BTGIVQ9Dom1";
       //document.getElementById("ar-link2").click();
         
     }
