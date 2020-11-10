e12963d75bb3de87af29b1ab5067a3e086e52dcd
AndrewPunchak <68635339+AndrewPunchak@users.noreply.github.com>
2020-11-09 21:17:56 -0800

    updaaaaaaaaaaates

--- js/presave.js
+++ js/presave.js
@@ -3,7 +3,7 @@
 
 document.getElementById("ar-link").addEventListener("message", function (event) {
     if (event.data == "_apple_ar_quicklook_button_tapped") {
-        window.location.href="https://open.spotify.com/artist/71p9YcOYtzkakGsABYYxKW";
+        window.location.href="https://google.com";
       //document.getElementById("ar-link2").click();
         
     }
